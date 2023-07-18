4f6e9f7a68dbe21750459ec5c034c6a3a74e7258
alik-balika <balikaalik@gmail.com>
2023-07-17 18:37:32 -0700

    setup connection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
-console.log(process.env.PORT);
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
